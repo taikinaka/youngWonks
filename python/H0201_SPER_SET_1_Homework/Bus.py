c6aa5015e5cb9176eeeeaa8be7b22f6017187f3c
Masashi Nakane <hypergori@gmail.com>
2019-04-21 06:29:19 -0700

    First Commmit

---
+++ python/H0201_SPER_SET_1_Homework/Bus.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print('Approximately how many students are there in each grade?')
+
+numberOfStudentsPerGrade = input()
+
+print('How many grades are going for the field trip?')
+
+numberOfGrades = input()
+
+print('What is the maximum capacity of each bus?')
+
+busCapacity = input()
+
+numberOfBuses = int(numberOfStudentsPerGrade) * int(numberOfGrades) /  int(busCapacity)
+
+numberOfStudents = int(numberOfStudentsPerGrade) * int(numberOfGrades)
+
+print('A total of',numberOfStudents, 'students will be going for the field trip.', numberOfBuses, 'buses will be needed.' )
