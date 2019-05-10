3be38a9fb7db90255846d72810d870f7e6b8caad
taikinaka <taiki.nakane@gmail.com>
2019-05-09 19:23:42 -0700

    HomeWork-2-1

---
+++ python/H0202_SPER_SET_2_Homework/simple_loop.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 18:54:03 2019
+
+@author: hyper
+"""
+
+#for a in range(0,22,1):
+#    print('Taiki')
+#b = 0
+#for a in range(0,25,1):
+    
+#    b = b + 2 
+#    print(b)
+#for a in range(0,22,1):
+#    print('Taiki')
+
+#for a in range(0,50): 
+#    if a % 2==1:
+#        print(a)
+
+#for a in range(1,31): 
+#    if a % 3==0:
+#        print(a)
+#for a in range(0,10):
+#    print('What is your name?')
+#    name = input()
+#    print('Hello',name)
+
+#for a in range(50,0,-1):
+#    print(a)
+
+for n in range(1,10,2):
+    print('I Love YoungWonks',n)
