234adefaadf73bc707389bb999227ce4f7e0e868
Taiki Nakane <taiki.nakane@gmail.com>
2019-05-22 08:08:50 -0700

    add dice prob.
    Description

---
+++ python/GAME/dice.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 19 14:10:21 2019
+
+@author: taikinaka
+"""
+
+print('Put numbers')
+N = int(input())
+A = 100 / N
+print(A,'%')
+
---
+++ python/GAME/diceQuaterGame.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 19 14:10:21 2019
+
+@author: taikinaka
+"""
+#print('put key')
+K = int(input())
+#print('Put numbers')
+prob = 0
+N = int(input())
+for a in range(1,N+1):
+    p = 0
+    num = a
+    while(K >num):
+        num=num*2
+        #print('num=',num)
+        p = p + 1
+    #print("p=",p)
+    prob = prob + 0.5**p
+prob = prob/N
+print(prob)
