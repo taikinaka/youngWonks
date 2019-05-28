17786ada7e493dfb4df8f5c9fbe76dd8ebb16d47
taikinaka <taiki.nakane@gmail.com>
2019-05-27 20:41:01 -0700

    homework3

---
+++ python/Homework 3/Making_Dictionary.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 27 12:04:13 2019
+
+@author: taiki
+"""
+
+dictionary = {'Kiminonawa': 4,'Detective Pikachu': 5,'Coco': 3,'Spyder-Man': 4,'Flash': 4}
+for movie in dictionary:
+    print(movie)
+    print('    Rating: ',dictionary[movie])
+    for a in range()
---
+++ python/Homework 3/loop3.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 27 20:37:17 2019
+
+@author: taiki
+"""
+
+name = 'Taiki Nakane'
+for a in range(0,12):
+    print(name[a])
